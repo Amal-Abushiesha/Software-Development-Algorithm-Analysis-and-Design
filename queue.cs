45c216c9c706c298fa5d662bb92b45b047819b53
Amal Hany <81009248+Amal-Abushiesha@users.noreply.github.com>
2023-03-17 13:19:54 +0200

    Stack and Queue Assignment

---
+++ queue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue_Assignment
+{
+    internal class queue
+    {
+
+        class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+        class Queue
+        {
+            private Node front, rear;
+            public Queue()
+            {
+                front = rear = null;
+            }
+
+            public void Enqueue(int data)
+            {
+                Node temp = new Node(data);
+                if (rear == null)
+                {
+                    front = rear = temp;
+                    return;
+                }
+                rear.next = temp;
+                rear = temp;
+            }
+
+            public void Dequeue()
+            {
+                if (front == null)
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                Node temp = front;
+                front = front.next;
+                if (front == null)
+                    rear = null;
+            }
+
+            public void PrintQueue()
+            {
+                if (front == null)
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                Node temp = front;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ stack.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue_Assignment
+{
+    internal class stack
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        public class Stack
+        {
+            private Node top;
+
+            public Stack()
+            {
+                this.top = null;
+            }
+
+            public void Push(int data)
+            {
+                Node newNode = new Node(data);
+
+                if (top == null)
+                {
+                    top = newNode;
+                    return;
+                }
+
+                newNode.next = top;
+                top = newNode;
+            }
+
+            public int Pop()
+            {
+                if (top == null)
+                {
+                    Console.WriteLine("Stack is empty");
+                    return -1;
+                }
+
+                int data = top.data;
+                top = top.next;
+                return data;
+            }
+
+            public int Peek()
+            {
+                if (top == null)
+                {
+                    Console.WriteLine("Stack is empty");
+                    return -1;
+                }
+
+                return top.data;
+            }
+
+            public bool IsEmpty()
+            {
+                return top == null;
+            }
+
+            public void PrintStack()
+            {
+                Node current = top;
+
+                while (current != null)
+                {
+                    Console.Write(current.data + " ");
+                    current = current.next;
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+
+
+
+    }
+
+}
